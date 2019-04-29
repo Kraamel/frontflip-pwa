2ea04813c818f56b2ef1416d99ea5806da5ef7c3
Kraamel <b.amel.braiek@gmail.com>
2019-04-29 15:07:19 +0200

    New card color

--- src/theme.js
+++ src/theme.js
@@ -53,7 +53,7 @@ export default createMuiTheme({
         padding: 8,
       },
       h5: {
-        color: 'white',
+        color:  'white',
         fontWeight: '700',
       },
       h4: {
@@ -61,7 +61,7 @@ export default createMuiTheme({
         fontWeight: '400',
       },
       body1: {
-        color: 'white',
+        color:  'white',
         fontWeight: '400',
         fontSize: '0.95rem',
       },
@@ -153,7 +153,8 @@ export default createMuiTheme({
         transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)',
         '&:hover': {
           boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
-        }
+        },
+        backgroundImage: 'linear-gradient(to bottom, #2b2d3c, #292a38, #262733, #24242f, #21212b)',
       }
     },
     MuiFab: {
@@ -180,7 +181,7 @@ export default createMuiTheme({
     MuiCardHeader: {
       root: {
         width: '100%',
-        backgroundColor: palette.primary.dark,
+        backgroundColor: "transparent",
       },
       content: {
         display: 'inline-grid',
